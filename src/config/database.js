4119403ce8116142a7af5c0fa472f4515a03834e
drebraga <dre.braga@gmail.com>
2023-01-24 22:19:02 -0300

    Versão inicial de projeto

---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+let db;
+
+try {
+    await mongoClient.connect();
+    db = mongoClient.db();
+} catch (err) {
+    console.log(err);
+}
+
+export default db;
