edcfa825709bc159606202e06cd83995add5c360
AlexandreDresch <z3r0pc@protonmail.com>
2023-01-27 20:57:51 -0300

    feat: GET cart & DELETE cart update

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -48,3 +48,43 @@ export const addToCart = async (req, res) => {
         return res.status(500).send(err.message);
     }
 }
+
+export const getCart = async (req, res) => {
+    const userId = res.locals.userId;
+
+    try {
+        const userCart = await db.collection("cart").find({ _id: ObjectId(userId) }).toArray();
+        return res.status(200).send(userCart);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export const updateCart = async (req, res) => {
+    
+}
+
+export const deleteCart = async (req, res) => {
+    const userId = res.locals.userId;
+
+    try {
+
+        const cart = await db.collection("cart").findOne({
+            userId: ObjectId(userId)
+        })
+
+        if (!cart) return res.status(400).send("User cart not found. contact the support.");
+
+        await db.collection("cart").updateOne(
+            {
+                userId: ObjectId(userId)
+            },
+            {
+                $set: {cart: []}
+            }            
+        );
+        return res.status(200).send("Cart content successfully deleted");
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { addToCart } from "../controller/cartController.js";
+import { addToCart, deleteCart, getCart, updateCart } from "../controller/cartController.js";
 import tokenValidation from "../middleware/tokenValidation.js"
 import { gameIdSchema } from "../schema/gameSchema.js";
 import schemaValidation from "../middleware/schemaValidation.js"
 
 const cartRouter = Router();
 
+cartRouter.get("/cart", tokenValidation(), getCart); //ad
 cartRouter.put("/cart", tokenValidation(), schemaValidation(gameIdSchema), addToCart);
+cartRouter.put("/cart-update", tokenValidation(), updateCart); //ad
+cartRouter.delete("/cart", tokenValidation(), deleteCart) //ad
 
 export default cartRouter;
