90996e677ccb725bf776b565778b24cf7daf8e98
drebraga <dre.braga@gmail.com>
2023-01-29 22:50:41 -0300

    fix: minor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import catalogRouter from "./routes/catalogRoutes.js"
 import authRouter from "./routes/authRoutes.js";
 import cartRouter from "./routes/cartRoutes.js";
 import checkoutRouter from "./routes/checkoutRoutes.js";
-
-dotenv.config();
 
 const app = express();
 
